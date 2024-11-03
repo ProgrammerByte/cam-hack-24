3647d6485b41d36b2c7f7278ff9e06a20ed23073
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2024-11-03 01:53:14 +0000

    Not on my watch Thomas

--- vite-frontend/src/routes/mobile/waitingPage.jsx
+++ vite-frontend/src/routes/mobile/waitingPage.jsx
@@ -26,17 +26,17 @@ const WaitingPage = () => {
     unassigned: "You are yet to be assigned to a team",
   };
 
-  const bgs = {
-    Scientists: "blue-500",
-    "Marine Corps": "green-500",
-    unassigned: "red-500",
-  };
-
   return (
     <div
-      className={`flex items-center justify-center h-screen bg-${bgs[team]}`}
+      className={
+        "flex items-center justify-center h-screen " +
+        (team === "Scientists"
+          ? "bg-blue-500"
+          : team === "Marine Corps"
+          ? "bg-green-500"
+          : "bg-red-500")
+      }
     >
-      <div className="hidden bg-blue-500 bg-green-500 bg-red-500" />
       <div className="text-center text-white font-bold">
         <div className="absolute top-0 w-full text-3xl my-4 left-0">
           <p>Your name is</p>
